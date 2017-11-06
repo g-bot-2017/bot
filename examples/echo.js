e6e1d2de13730026bddcaa7338a4e53db1e4f0e8
g-bot-2017 <33308781+g-bot-2017@users.noreply.github.com>
2017-11-06 09:53:39 +0700

    Update echo.js

--- examples/echo.js
+++ examples/echo.js
@@ -1,9 +1,9 @@
 const linebot = require('../index.js');
 
 const bot = linebot({
-	channelId: process.env.1544329889,
-	channelSecret: process.env.59844eb05fa2946355afe5dc6fd46a37,
-	channelAccessToken: process.env.jlK1k3fIaNjzQMFFAz1IcpQymHMFX8hu6KePYX/Y+kOwAyN4ikp1tqPOCAOaLzotpt53+sMSgNxidVeYiUzcEwjrveRxDe2q1yZ96ebyxECpqTWjt5h9GzzcD/T1iUFceu8W4hvfqYpyJcANaVyLuAdB04t89/1O/w1cDnyilFU=
+	channelId: process.env.CHANNEL_ID,
+	channelSecret: process.env.CHANNEL_SECRET,
+	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
 bot.on('message', function (event) {
